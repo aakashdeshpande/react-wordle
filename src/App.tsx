156547d11b8e444358cdc6922705602a0294757b
Aakash Deshpande <aakashd10@gmail.com>
2022-02-11 12:06:20 +0530

    Move image to top

--- src/App.tsx
+++ src/App.tsx
@@ -213,6 +213,8 @@ function App() {
     <div className="pt-2 pb-8 max-w-7xl mx-auto sm:px-6 lg:px-8">
       <div className="flex w-80 mx-auto items-center mb-8 mt-20">
         <img alt='profile' style={{ width: 100 }} src={String(profilePic)} />
+      </div>
+      <div className="flex w-80 mx-auto items-center mb-8 mt-20">
         <h1 className="text-xl ml-2.5 grow font-bold dark:text-white">
           {GAME_TITLE}
         </h1>
