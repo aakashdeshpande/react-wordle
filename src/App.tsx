71b63f7bceee3683abcac64200177d6f206a6c74
Aakash Deshpande <aakashd10@gmail.com>
2022-02-11 11:07:18 +0530

    Fix profile photo location

--- src/App.tsx
+++ src/App.tsx
@@ -44,7 +44,7 @@ import {
 import './App.css'
 
 function App() {
-  const profilePic = require('../public/IMG-4142.jpeg');
+  const profilePic = require('profile-pic.jpeg');
   
   const prefersDarkMode = window.matchMedia(
     '(prefers-color-scheme: dark)'
