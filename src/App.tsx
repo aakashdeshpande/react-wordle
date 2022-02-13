1518b4db184610ffb866eecd0aeb375ab50b78c8
Aakash Deshpande <aakashd10@gmail.com>
2022-02-13 23:45:58 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -216,6 +216,7 @@ function App() {
   return (
     <div className="pt-2 pb-8 max-w-7xl mx-auto sm:px-6 lg:px-8">
       <div className="flex w-80 mx-auto items-center mb-8 mt-20">
+        <img alt='profile' style={{ width: 100 }} src={String(require('./profile-pic.jpeg'))} />
         <h1 className="text-xl ml-2.5 grow font-bold dark:text-white">
           {GAME_TITLE}
         </h1>
